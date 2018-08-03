dafc47066767121980066ac762af3ce73401385d
subhamproject <subhammandal01@gmail.com>
2018-08-03 15:26:19 +0530

    Update build.sh

--- SBI/build.sh
+++ SBI/build.sh
@@ -5,5 +5,12 @@ export USERID=$(id -u)
 export GROUPID=$(id -g)
 echo "Running as UID=$USERID, GID=$GROUPID"
 cd $(dirname $0)
+CONTAINER_NAME="builder-$(echo ${JOB_NAME} | tr '/ ' '._').${BRANCH_NAME}"
+if [ -n "$CHANGE_ID" ]
+then
+    CONTAINER_NAME="${CONTAINER_NAME}-PR${CHANGE_ID}"
+fi
+CONTAINER_NAME="${CONTAINER_NAME}-${BUILD_ID}"
+
 
 docker-compose -f test-bed.yml run --name maven-${BUILD_NUMBER} --rm -w "$WORKSPACE" --entrypoint "mvn package -DskipTests" maven-app-build
